49d9c0dd5c1a8f8ed9b4983454e431c5e42f58ad
Fabián Erubiel Rojas Yañez <a01706636@itesm.mx>
2022-09-16 22:40:56 -0500

    Merge

--- memory.py
+++ memory.py
@@ -8,25 +8,27 @@ Exercises:
 4. Center single-digit tile.
 5. Use letters instead of tiles.
 """
-
+# Declaramos inidvidualmente los modulos importados
 from random import shuffle
 from turtle import tracer, hideturtle, addshape, setup, stamp
 from turtle import ontimer, update, write, goto, up, begin_fill
 from turtle import shape, clear, end_fill, left, forward
 from turtle import color, down, done, onscreenclick
-
 from freegames import path
 
+# Declaracion de variables
 car = path('car.gif')
 tiles = list(range(32)) * 2
 state = {'mark': None}
 hide = [True] * 64
-terminado = False
-counter = 0
+# Contadores
+numTaps = 0
+pares = 0
+fin = False
 
 
 def square(x, y):
-    """Draw white square with black outline at (x, y)."""
+    "Draw white square with black outline at (x, y)."
     up()
     goto(x, y)
     down()
@@ -39,19 +41,22 @@ def square(x, y):
 
 
 def index(x, y):
-    """Convert (x, y) coordinates to tiles index."""
+    "Convert (x, y) coordinates to tiles index."
     return int((x + 200) // 50 + ((y + 200) // 50) * 8)
 
 
 def xy(count):
-    """Convert tiles count to (x, y) coordinates."""
+    "Convert tiles count to (x, y) coordinates."
     return (count % 8) * 50 - 200, (count // 8) * 50 - 200
 
 
 def tap(x, y):
-    """Update mark and hidden tiles based on tap."""
+    "Update mark and hidden tiles based on tap."
     spot = index(x, y)
     mark = state['mark']
+    global numTaps
+    global pares
+    global fin
 
     if mark is None or mark == spot or tiles[mark] != tiles[spot]:
         state['mark'] = spot
@@ -59,10 +64,23 @@ def tap(x, y):
         hide[spot] = False
         hide[mark] = False
         state['mark'] = None
+        pares += 1
+
+
+# Contador de los clicks que se da en la pantalla
+    numTaps += 1
+    print("Numero total de clicks ", numTaps)
+    print("Pares enocntrados: ", pares)
+    print("")
+# Indica cuando se enontraon todos los pares de cartas
+    if (pares == 32):
+        print("Ganaste!!!!!")
+        fin = True
+# Termina el juego
 
 
 def draw():
-    """Draw image and tiles."""
+    "Draw image and tiles."
     clear()
     goto(0, 0)
     shape(car)
@@ -78,9 +96,12 @@ def draw():
     if mark is not None and hide[mark]:
         x, y = xy(mark)
         up()
-        goto(x + 2, y)
-        color('black')
-        write(tiles[mark], font=('Arial', 30, 'normal'))
+        goto(x+25, y+10)
+
+        write(tiles[mark], align='center', font=('Arial', 15, 'bold'))
+
+    if (fin):
+        return
 
     update()
     ontimer(draw, 100)
@@ -94,7 +115,3 @@ tracer(False)
 onscreenclick(tap)
 draw()
 done()
-# El dibujo ha sido completado
-terminado = True
-print(terminado)
-print(counter)
